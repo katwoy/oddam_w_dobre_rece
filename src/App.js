59cad9750e19fcbf5bbc22d35c3bfe878f0b8401
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-24 19:59:45 +0100

    Add styles to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './scss/App.scss';
 import Home from './components/Home/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Logout from './components/Logout';
+import {
+  HashRouter,
+  Route,
+  Switch,
+  } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Home />
+    <div className="app__container">
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/logowanie' component={Login} />
+          <Route path='/rejestracja' component={Register} />
+          <Route path='/wylogowano' component={Logout} />
+        </Switch>
+      </HashRouter>
+    </div>
   );
 }
 
--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -5,8 +5,19 @@ import '../../scss/HomeHeader.scss';
 const HomeHeader = () => {
   return (
     <header className="header">
-      <Link to="/logowanie"><button className="header__button">Zaloguj</button></Link>
-      <Link to="/rejestracja"><button className="header__button">Załóż konto</button></Link>
+      <div className="header__buttons">
+        <Link to="/logowanie"><button className="header__button">Zaloguj</button></Link>
+        <Link to="/rejestracja"><button className="header__button">Załóż konto</button></Link>
+      </div>
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__li">Start</li>
+          <li className="header__li">O co chodzi?</li>
+          <li className="header__li">O nas</li>
+          <li className="header__li">Fundacje i organizacje</li>
+          <li className="header__li">Kontakt</li>
+        </ul>
+      </nav>
     </header>
   )
 }
