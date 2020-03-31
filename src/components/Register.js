ab201fafc8b5c8bf6fc6e7881fa58a3b53350a5b
Katarzyna Wojcicka <katwoy@gmail.com>
2020-03-31 16:47:52 +0200

    Add registration form layout

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import '../scss/Register.scss';
+import HomeTitle from './Home/HomeTitle';
 
 const Register = () => {
   return (
-    <div>rejestracja</div>
+    <section className="register">
+      <HomeTitle title={"Załóż konto"} />
+      <form className="register__form">
+        <label>Email
+          <input type="email" name="email"/>
+        </label>
+        <label>Hasło
+          <input type="password" name="password"/>
+        </label>
+        <label>Powtórz hasło
+          <input type="password" name="password2"/>
+        </label>
+      </form>
+      <div className="register__buttons">
+      <Link to="/logowanie" className="register__button">Zaloguj się</Link>
+      <Link to="/rejestracja" className="register__button active">Załóż konto</Link>
+      </div>
+    </section>
   )
 }
 
