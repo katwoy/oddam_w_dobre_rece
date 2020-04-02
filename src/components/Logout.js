6ac79edb27d8ea8d1219fe111409691a9d4e8a4d
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-02 06:11:43 +0200

    Add logout layout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import '../scss/Logout.scss';
+import HomeTitle from './Home/HomeTitle';
 
 const Logout = () => {
   return (
-    <div>wylogowanie</div>
+    <section className="logout">
+      <div className="logout__container">
+        <HomeTitle title={"Wylogowanie nastąpiło pomyślnie!"} />
+        <Link to="/" className="logout__button">Strona główna</Link>
+      </div>
+    </section>
   )
 }
 
