e2bf5932c683dbfb0b05b2a7d919c145c1404845
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-09 07:21:50 +0200

    Add elements to Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import '../scss/Form.scss';
-import HomeTitle from './Home/HomeTitle';
 import formHero from '../assets/Form-Hero-Image.jpg';
 
 const Form = () => {
   return (
     <section className="form">
-      <img src={formHero} alt="green sweater" className="form__img"/>
-      <div className="form__container">
-        <HomeTitle title={"Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM"} />
-        <div>Wystarczą 4 proste kroki</div>
+      <div className="form__img">
+        <img src={formHero} alt="green sweater"/>
+      </div> 
+      <div className="form__description">
+        <h1 className="form__title">Oddaj rzeczy, których już nie chcesz<br/><span>potrzebującym</span></h1>
+        <div className="form__subtitle">Wystarczą 4 proste kroki:</div>
+        <div className="form__steps">
+          <div>1<br/>Wybierz rzeczy</div>
+          <div>2<br/>Spakuj je w worki</div>
+          <div>3<br/>Wybierz fundację</div>
+          <div>4<br/>Zamów kuriera</div>
+        </div>
       </div>
     </section>
   )
