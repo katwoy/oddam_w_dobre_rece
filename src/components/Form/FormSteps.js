ed29fb134647c6bbe33f257e5f963e7ebea93fee
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-18 19:18:41 +0200

    Add first step of form

--- src/components/Form/FormSteps.js
+++ src/components/Form/FormSteps.js
@@ -3,10 +3,33 @@ import '../../scss/FormSteps.scss';
 
 const FormSteps = () => {
   return (
-    <form className="form">
+    <section className="form__steps">
       <div className="form__subtitle">Krok 1/4</div>
       <h3 className="form__title">Zaznacz co chcesz oddać:</h3>
-    </form>
+      <form className="form__fields">
+        <label>
+          <input type="checkbox" name="what" value="goodclothes"></input>
+          ubrania, które nadają się do ponownego użycia
+        </label>
+        <label>
+          <input type="checkbox" name="what" value="badclothes"></input>
+          ubrania do wyrzucenia
+        </label>
+        <label>
+          <input type="checkbox" name="what" value="toys"></input>
+          zabawki
+        </label>
+        <label>
+          <input type="checkbox" name="what" value="books"></input>
+          książki
+        </label>
+        <label>
+          <input type="checkbox" name="what" value="other"></input>
+          inne
+        </label>
+      </form>
+      <button className="form__button">Dalej</button>
+    </section>
   )
 }
 
