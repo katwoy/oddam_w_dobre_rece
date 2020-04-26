caae07b6e8da435c42144058f10bc71129d0e97d
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-26 17:04:37 +0200

    Divide form stylesheet to steps

--- src/components/Form/FormStep1.js
+++ src/components/Form/FormStep1.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import '../../scss/FormStep1.scss';
 
 const FormStep1 = () => {
 
   return (
     <>
-      <div className="form__subtitle">Krok 1/4</div>
+      <div className="form__counter">Krok 1/4</div>
       <h3 className="form__title">Zaznacz co chcesz oddać:</h3>
       <form className="form__fields">
-        <label>
-          <input type="checkbox" name="what" value="goodclothes"></input>
-          ubrania, które nadają się do ponownego użycia
-        </label>
-        <br/>
-        <label>
-          <input type="checkbox" name="what" value="badclothes"></input>
-          ubrania do wyrzucenia
-        </label>
-        <br/>
-        <label>
-          <input type="checkbox" name="what" value="toys"></input>
-          zabawki
-        </label>
-        <br/>
-        <label>
-          <input type="checkbox" name="what" value="books"></input>
-          książki
-        </label>
-        <br/>
-        <label>
-          <input type="checkbox" name="what" value="other"></input>
-          inne
-        </label>
+        <div className="form__what">
+          <label>
+            <input type="checkbox" name="what" value="goodclothes"></input>
+            ubrania, które nadają się do ponownego użycia
+          </label>
+          <br/>
+          <label>
+            <input type="checkbox" name="what" value="badclothes"></input>
+            ubrania do wyrzucenia
+          </label>
+          <br/>
+          <label>
+            <input type="checkbox" name="what" value="toys"></input>
+            zabawki
+          </label>
+          <br/>
+          <label>
+            <input type="checkbox" name="what" value="books"></input>
+            książki
+          </label>
+          <br/>
+          <label>
+            <input type="checkbox" name="what" value="other"></input>
+            inne
+          </label>
+        </div>
       </form>
       <div className="form__buttons">
         <button className="form__button">Dalej</button>
--- src/components/Form/FormStep2.js
+++ src/components/Form/FormStep2.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import '../../scss/FormStep2.scss';
 
 const FormStep2 = () => {
   return (
     <>
-      <div className="form__subtitle">Krok 2/4</div>
+      <div className="form__counter">Krok 2/4</div>
       <h3 className="form__title">Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</h3>
       <form className="form__fields">
         <span className="form__text">Liczba 60l worków:</span>
-        <div className="select">
+        <div className="form__amount">
           <select>
             <option value="0">- wybierz -</option>
             <option value="1">1</option>
