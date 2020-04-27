5931df57e346772e3381c65b5900e90d327a50e1
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-27 07:02:25 +0200

    Finish styling if the third step

--- src/components/Form/FormStep3.js
+++ src/components/Form/FormStep3.js
@@ -19,24 +19,29 @@ const FormStep2 = () => {
         </div>
         <h4 className="form__subtitle">Komu chcesz pomóc?</h4>
         <div className="form__who">
-          <label>dzieciom
+          <label>
             <input type="checkbox" name="who" value="children"></input>
+            <span>dzieciom</span>
           </label>
-          <label>samotnym matkom
+          <label>
             <input type="checkbox" name="who" value="singlemothers"></input>
+            <span>samotnym matkom</span>
           </label>
-          <label>bezdomnym
+          <label>
             <input type="checkbox" name="who" value="homeless"></input>
+            <span>bezdomnym</span>
           </label>
-          <label>niepełnosprawnym
+          <label>
             <input type="checkbox" name="who" value="disabled"></input>
+            <span>niepełnosprawnym</span>
           </label>
-          <label>osobom starszym
+          <label>
             <input type="checkbox" name="who" value="elderly"></input>
+            <span>osobom starszym</span>
           </label>
         </div>
-        <div>
-          <h4>Wpisz nazwę konkretnej organizacji (opcjonalnie)</h4>
+        <div className="form__organisation">
+          <h4 className="form__subtitle">Wpisz nazwę konkretnej organizacji (opcjonalnie)</h4>
           <textarea></textarea>
         </div>
       </form>
