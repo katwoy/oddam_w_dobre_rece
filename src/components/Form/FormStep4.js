c92d989b84659298f7ae38034077cd75671e71e5
Katarzyna Wojcicka <katwoy@gmail.com>
2020-04-30 16:09:50 +0200

    Add step 4 to the form

---
+++ src/components/Form/FormStep4.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import '../../scss/FormStep4.scss';
+
+const FormStep4 = () => {
+  return (
+    <>
+      <div className="form__counter">Krok 4/4</div>
+      <h3 className="form__title">Podaj adres oraz termin odbioru rzeczy przez kuriera</h3>
+      <form className="form__fields">
+        <div className="form__pickup">
+          <div className="form__address">
+            <h4>Adres odbioru:</h4>
+            <label>
+              <span>Ulica</span>
+              <input type="text" name="street"></input>
+            </label>
+            <label>
+              <span>Miasto</span>
+              <input type="text" name="city"></input>
+            </label>
+            <label>
+              <span>Kod pocztowy</span>
+              <input type="text" name="postalcode"></input>
+            </label>
+            <label>
+              <span>Numer telefonu</span>
+              <input type="tel" name="phone"></input>
+            </label>
+          </div>
+          <div className="form__date">
+            <h4>Termin odbioru:</h4> 
+            <label>
+              <span>Data</span>
+              <input type="date" name="date"></input>
+            </label>
+            <label>
+              <span>Godzina</span>
+              <input type="time" name="time"></input>
+            </label>
+            <label>
+              <span>Uwagi dla kuriera</span>
+              <textarea name="notes"></textarea>
+            </label>
+          </div>
+        </div>
+      </form>
+      <div className="form__buttons">
+        <button className="form__button">Wstecz</button>
+        <button className="form__button">Dalej</button>
+      </div>
+    </>
+  )
+}
+
+export default FormStep4;
--- src/components/Form/FormSteps.js
+++ src/components/Form/FormSteps.js
@@ -3,7 +3,8 @@ import '../../scss/FormSteps.scss';
 import FormInfo from './FormInfo';
 // import FormStep1 from './FormStep1';
 // import FormStep2 from './FormStep2';
-import FormStep3 from './FormStep3';
+// import FormStep3 from './FormStep3';
+import FormStep4 from './FormStep4';
 
 const FormSteps = () => {
 
@@ -11,7 +12,7 @@ const FormSteps = () => {
     <>
       <FormInfo />
       <section className="form__steps">
-        <FormStep3 />
+        <FormStep4 />
       </section>
     </>
   )
