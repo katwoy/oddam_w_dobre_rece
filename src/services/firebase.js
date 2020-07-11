106983810d69b2b443ebe35a8d8acee66af61c58
Katarzyna Wojcicka <katwoy@gmail.com>
2020-07-11 13:39:06 +0200

    Hide keys

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,15 @@ import * as firebase from 'firebase';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGHqWXXY0CyPQG-yYV_YWT5uFAXUeIxDk",
-  authDomain: "oddam-w-dobre-rece-ee814.firebaseapp.com",
-  databaseURL: "https://oddam-w-dobre-rece-ee814.firebaseio.com",
-  projectId: "oddam-w-dobre-rece-ee814",
-  storageBucket: "oddam-w-dobre-rece-ee814.appspot.com",
-  messagingSenderId: "1053539237898",
-  appId: "1:1053539237898:web:d6d424840fe0e3ec38dff3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth;
